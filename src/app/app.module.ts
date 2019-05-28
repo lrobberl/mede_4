f358797d0e03b2c5197d02bd496929bfc2991c10
Michelangelo Moncada <s255544@studenti.polito.it>
2019-05-28 11:44:11 +0200

    modulo per la grafica inserito, sidenav inserito (finestra mobile ok)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MatListModule } from '@angular/material/list';
-import {MatCardModule, MatIconModule, MatPaginatorModule, MatRadioModule} from '@angular/material';
+import {MatCardModule, MatIconModule, MatPaginatorModule, MatRadioModule, MatToolbarModule, MatButtonModule, MatSidenavModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {PedibusComponent} from './pedibus.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 
 @NgModule({
   declarations: [
-    PedibusComponent
+    PedibusComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatListModule,
     MatIconModule,
     MatRadioModule,
     MatCardModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [PedibusComponent]
