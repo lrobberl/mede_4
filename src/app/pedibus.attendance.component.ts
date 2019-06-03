24d92d34c04f754666e195b3b5b6a707b27f48c7
Dario <s252642@studenti.polito.it>
2019-06-03 21:18:59 +0200

    Sistemata la visualizzazione integrando le chiamate al DB. Problemi: - Non vede i bambini - Premendo sulle linee diverse dalla rossa non visualizza niente (dovuto al radio botton che non prende il valore)

--- src/app/pedibus.attendance.component.ts
+++ src/app/pedibus.attendance.component.ts
@@ -2,32 +2,70 @@ import {Component, OnInit} from '@angular/core';
 import {MatRadioChange, PageEvent} from '@angular/material';
 import {DomSanitizer} from '@angular/platform-browser';
 import {MatIconRegistry} from '@angular/material';
-import { HttpService } from './pedibusHTTP.service';
+import {Bambino, Corsa, HttpService} from './pedibusHTTP.service';
+import {Fermata, Persona} from './pedibus.component';
+import {Observable} from 'rxjs';
 
+export interface Linea {
+  Nome: string;
+  fermate: Fermata[];
+}
+
+export interface Bambino {
+  id: bigint;
+  nome: string;
+  presente: boolean;
+}
+
+export interface Corsa {
+  fermate: Fermata[];
+  nomeVerso: string;
+}
+
+export interface Fermata {
+  nome: string;
+  orario: string;
+  bambini: Bambino[];
+}
+
+export interface Data {
+  date: Date;
+  linea: string;
+  corse: Corsa[];
+}
 
 @Component({
   selector: 'app-pedibus-attendance',
-  templateUrl: './pedibus.component.html',
-  styleUrls: ['./pedibus.component.css']
+  templateUrl: './pedibus.attendance.component.html',
+  styleUrls: ['./pedibus.attendance.component.css']
 })
 
 export class PedibusAttendanceComponent implements OnInit {
   title = 'Esercitazione - #5';
-  data$;
+  data$: Observable<Data>;
+  selectedDate: string;
+  linee$: Observable<Linea[]>;
   item$;
+
 
   constructor(private httpService: HttpService) {
   }
 
   ngOnInit() {
+    this.linee$ = this.httpService.getLines();
     const date = new Date();
-    this.data$ = this.httpService.getCorsa('rossa', date.getDay().toString() + date.getMonth().toString() + date.getFullYear().toString());
+    this.selectedDate = date.getDay().toString() + date.getMonth().toString() + date.getFullYear().toString();
+    this.data$ = this.httpService.getCorsa('Rossa', this.selectedDate);
   }
 
   getCorsa(linea: string, data: string) {
+    this.selectedDate = data;
     this.data$ = this.httpService.getCorsa(linea, data);
   }
 
+  segnaPresente($event: MouseEvent, persona: Bambino) {
+    persona.presente = (persona.presente === true) ? false : true;
+  }
 
 
   /*getAll() {
