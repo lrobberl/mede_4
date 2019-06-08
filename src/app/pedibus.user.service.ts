0b35ec4d64aebd8e23e5e537367b883a4081725e
Dario <s252642@studenti.polito.it>
2019-06-08 13:11:49 +0200

    Piccola modifica per merge

--- src/app/pedibus.user.service.ts
+++ src/app/pedibus.user.service.ts
@@ -11,34 +11,38 @@ const httpOptions = {
   })
 };
 
+export interface User {
+  name: string;
+  surname: string;
+  email: string;
+  pass: string;
+  pass2: string;
+}
+
 @Injectable()
 export class UserService {
+  user: User;
   constructor(private http: HttpClient) {
   }
 
-  register(firstName: string, lastName: string, mail: string, pass: string, pass2: string) {
+  register(firstName: string, lastName: string, mail: string, password: string, password2: string): Observable<User> {
     console.log('UserService.register:');
 
     const httpoptions = { headers: new HttpHeaders({
         'Content-Type':  'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Authorization, X-Requested-With, Content-Type, Accept, Origin',
-        'Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS',
-        'Access-Control-Max-Age': '1728000'
       })
     };
 
-    const bodyObj = {
-      nome: firstName,
-      cognome: lastName,
+    this.user = {
+      name: firstName,
+      surname: lastName,
       email: mail,
-      password: pass,
-      password2: pass2,
-    };
+      pass: password,
+      pass2: password2
+  };
+    const body = JSON.stringify(this.user);
 
-    const body = JSON.stringify(bodyObj);
-
-    return this.http.post(REST_URL + 'register', body, httpoptions).pipe(
+    return this.http.post<User>(REST_URL + 'register', body, httpoptions).pipe(
       catchError(err => {
         console.error(err);
         return of(null);
